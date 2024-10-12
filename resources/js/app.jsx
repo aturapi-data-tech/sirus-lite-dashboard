3ea5c525f07ac93355963e9a0a03058b191b84d5
SYAMS. info <nuurwahidanshary@gmail.com>
2024-10-12 09:33:52 +0700

    ok

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,7 +7,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { Provider } from 'react-redux';
 import store from '@/redux/store';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Sirus';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
